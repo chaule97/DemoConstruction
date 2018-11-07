b85adf9bc97d60426ee859fa78cc9042cfda14f7
Duc Anh <tdanhdev@gmail.com>
2018-11-07 13:26:00 +0700

    fix

--- frontend/src/ui/components/ProjectPage/ProjectPage.jsx
+++ frontend/src/ui/components/ProjectPage/ProjectPage.jsx
@@ -66,98 +66,98 @@ class ProjectPage extends React.Component {
     }
 
     onShowGridView = () => {
-        this.setState({viewGridStatus: true})
+        this.setState({ viewGridStatus: true })
     }
 
     onShowListView = () => {
-        this.setState({viewGridStatus: false})
+        this.setState({ viewGridStatus: false })
     }
     render() {
-        const {viewGridStatus} = this.state;
+        const { viewGridStatus } = this.state;
         const { listProjects } = this.props;
         return (
             <div>
                 <div className="pull-right m-t-s25 m-r-20 ">
-                    <button  className={viewGridStatus ? "btn-transparent font-25": "btn-transparent"} 
-                        onClick = {() => this.onShowGridView()}
+                    <button className={viewGridStatus ? "btn-transparent font-25" : "btn-transparent"}
+                        onClick={() => this.onShowGridView()}
                     ><i class="fa fa-th-large"></i></button>
-                    <button  className={!viewGridStatus ? "btn-transparent font-25": "btn-transparent"}
-                         onClick = {() => this.onShowListView()}
+                    <button className={!viewGridStatus ? "btn-transparent font-25" : "btn-transparent"}
+                        onClick={() => this.onShowListView()}
                     ><i class="fa fa-list"></i></button>
-    
-                </div>
-            { viewGridStatus ? 
-            <section className="content">
-                <div className="row">
-                    {listProjects.map((item, index) => {
-                        return (
-                            <span key={index} className="cursor-pointer" onClick={() => this.viewDashboard()}>
-                                <div className="col-md-3 col-sm-6 col-xs-12">
-                                    <Link to={'/project/detail'}>
-                                        <div className="info-box">
-                                            <span className="info-box-icon bg-aqua"><i className="fa fa-cogs"></i></span>
 
-                                            <div className="info-box-content">
-                                                <span className="info-box-text">{item.name}</span>
-                                                <span className="info-box-number">{item.supervisor.username}</span>
-                                            </div>
-                                        </div>
-                                    </Link>
-                                </div>
-                            </span>
-                        )
-                    })}
                 </div>
-            </section>
-            :
-             <div>             
-             <section className="content">
-             <div className="row">
-               <div className="col-xs-12">
-                 <div className="box">
-                 <div className="box-header with-border">
-                 <h3 className="box-title">
-                 <i className="fa fa-user m-r-5"></i>
-                   <i className="fa m-r-5"></i> Dự án</h3>
-              
-                 </div>
-                   <div className="box-body">
-                     <table className="table table-bordered table-striped">
-                       <thead>
-                         <tr>
-                           <th>Tên dự án</th>
-                           <th>Giám sát</th>
-                           <th>Ngày khởi tạo</th>
-                           <th></th>
-                         </tr>
-                       </thead>
-                       <tbody>
-                       {listProjects.map((item, index) => {
-                        return (
-                            <tr key={index}>
-                                <td>{item.name}</td>
-                                <td>{item.supervisor.username}</td>
-                                <td>{moment().format("DD-MM-YYYY")}</td>
-                                <td>
-                                    <Button color={'info'}>Chi tiết</Button> &nbsp;
+                {viewGridStatus ?
+                    <section className="content">
+                        <div className="row">
+                            {listProjects.map((item, index) => {
+                                return (
+                                    <span key={index} className="cursor-pointer" onClick={() => this.viewDashboard()}>
+                                        <div className="col-md-3 col-sm-6 col-xs-12">
+                                            <Link to={'/project/detail'}>
+                                                <div className="info-box">
+                                                    <span className="info-box-icon bg-aqua"><i className="fa fa-cogs"></i></span>
+
+                                                    <div className="info-box-content">
+                                                        <span className="info-box-text">{item.name}</span>
+                                                        <span className="info-box-number">{item.supervisor.username}</span>
+                                                    </div>
+                                                </div>
+                                            </Link>
+                                        </div>
+                                    </span>
+                                )
+                            })}
+                        </div>
+                    </section>
+                    :
+                    <div>
+                        <section className="content">
+                            <div className="row">
+                                <div className="col-xs-12">
+                                    <div className="box">
+                                        <div className="box-header with-border">
+                                            <h3 className="box-title">
+                                                <i className="fa fa-user m-r-5"></i>
+                                                <i className="fa m-r-5"></i> Dự án</h3>
+
+                                        </div>
+                                        <div className="box-body">
+                                            <table className="table table-bordered table-striped">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Tên dự án</th>
+                                                        <th>Giám sát</th>
+                                                        <th>Ngày khởi tạo</th>
+                                                        <th></th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    {listProjects.map((item, index) => {
+                                                        return (
+                                                            <tr key={index}>
+                                                                <td>{item.name}</td>
+                                                                <td>{item.supervisor.username}</td>
+                                                                <td>{moment().format("DD-MM-YYYY")}</td>
+                                                                <td>
+                                                                    <Button color={'info'}>Chi tiết</Button> &nbsp;
                                     <Button color={'success'}>Báo cáo</Button>
-                                </td>
-                            </tr>
-                        )
-                        })}
-                          
-                       </tbody>
-                     </table>
-                   </div>
-                 </div>
-               </div>
-             </div>
-             </section>
-         </div>}
-         <div className="right">
+                                                                </td>
+                                                            </tr>
+                                                        )
+                                                    })}
+
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>}
+                <div className="right">
                     <Button color={'success'} onClick={() => this.props.createProject()}>Tạo dự án</Button>
-                </div> 
-         </div>
+                </div>
+            </div>
         );
     }
 }
